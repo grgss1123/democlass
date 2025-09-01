b7cb0d3f41e3178185e9ef6d7876d02f72fa8a1b
grgss1123 <sauravgrggg@gmail.com>
2025-09-01 14:05:07 +0545

    bananan

---
+++ seven.py
@@ -0,0 +1,18 @@
+class animal:
+    def __init__(self,name,breed):
+        self.name = name
+        self.breed = breed
+    def display_info(self):
+            print(f"name = {self.name}, breed ={self.breed})
+        
+
+class cat(animal):
+     
+
+
+
+
+ 
+            
+       
+        
