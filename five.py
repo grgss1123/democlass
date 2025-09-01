adb60af124b93878640e3c9ff23d97ba775fdd03
grgss1123 <sauravgrggg@gmail.com>
2025-09-01 13:08:08 +0545

    apple

---
+++ five.py
@@ -0,0 +1,6 @@
+number= int(input("enter a number"))
+i=int(1)
+while i<11:
+    a= number*i
+    print(f" {number}x{i}: {a} ")
+    i +=1
---
+++ four.py
@@ -0,0 +1,7 @@
+student={}
+for i in range(0,3):
+name= input("enter a name")
+grades= input("enter your grades")
+student[name] = grades
+print (student)
+ 
